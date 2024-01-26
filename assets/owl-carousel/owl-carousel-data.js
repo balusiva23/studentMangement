4ef6ed4844c4556841257a9b36a912f8cac1ee61
Sivakumar <balusiva2312@gmail.com>
2024-01-26 17:43:07 +0530

    new commit changes 2

--- assets/owl-carousel/owl-carousel-data.js
+++ assets/owl-carousel/owl-carousel-data.js
@@ -8,7 +8,7 @@ $(document).ready(function() {
 				});
 				$("#owl-demo2").owlCarousel({
 					autoPlay: 4000,
-					items: 3,
+					items: 4,
 					itemsDesktop: [1199, 3],
 					itemsDesktopSmall: [979, 3]
 				});
